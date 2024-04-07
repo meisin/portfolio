3aec49101c10930338e9fd5d398c4419a1b0bb98
Meisin Lee <meisin123@gmail.com>
2024-04-07 15:37:16 +0800

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import linkedin from "./linkedin.png";
+import linkedin from "./LinkedIn.png";
 import github_light from "./github_light.png";
 
 import docker from "./tech/docker.png";
